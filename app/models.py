7424aeee342563551c3cffdc3b3c171e7d1227b8
Michal-Barcak <michal.barcak@gmail.com>
2025-05-22 11:53:47 +0200

    feat: add model configand field validation

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel, Field, field_validator
 
 class OrderBase(BaseModel):
-    customer_name: str
+    customer_name: str = Field(..., min_length=1)
     price: float = Field(..., gt=0)
-    currency: str
+    currency: str = Field(..., min_length=1)
 
     @field_validator('currency')
     def currency_valid(cls, currency):
@@ -11,6 +11,17 @@ class OrderBase(BaseModel):
         if currency not in allowed_currencies:
             raise ValueError(f"Currency must to be one of allowed currencies: {allowed_currencies}")
         return currency
+    
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "customer_name": "John Doe",
+                "price": 99.99,
+                "currency": "CZK",
+                "id": 1
+            }
+        }
+    }
 
 class OrderCreate(OrderBase):
     pass
