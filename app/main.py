0a9f5857623544989d8520cd6cc34822f9a23e18
Michal-Barcak <michal.barcak@gmail.com>
2025-05-22 11:24:45 +0200

    feat: method create order

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from .database import init_db, get_db_connection
+from . import models
+
+
+init_db()
+
+app = FastAPI(title="Order Management API", version="1.0.0")
+
+
+@app.get("/")
+def root():
+    return {"message": "Order Management"}
+
+@app.post("/orders", response_model=models.Order, status_code=201)
+def create_order(order: models.OrderCreate):
+    """Create new order"""
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO orders (customer_name, price, currency) VALUES (?,?,?)",
+            (order.customer_name, order.price, order.currency)
+        )
+        conn.commit()
+        order_id = cursor.lastrowid
+
+        cursor.execute(
+            "SELECT id, customer_name, price, currency FROM orders WHERE id = ?",
+            (order_id,)
+        )
+        result = dict(cursor.fetchone())
+
+    return result
+          
